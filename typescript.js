36847add413567046b9e0dde7ed8379c69954632
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-03-06 16:27:56 +0100

    installing and using typescript

---
+++ typescript.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+var res = add(2, 2);
+console.log(res);
---
+++ typescript.ts
@@ -0,0 +1,6 @@
+function add(a:number,b:number){
+    return a+b
+}
+
+const res = add(2,2)
+console.log(res)
