a6bdd0a527880081e47c16c3b8469c06fc173086
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-03-10 21:03:29 +0100

    exercise

--- react-ts/src/models/todo.ts
+++ react-ts/src/models/todo.ts
@@ -7,21 +7,4 @@ class Todo {
         this.id = new Date().toISOString()
     }
 }
-
-
-class Num{
-    x: number;
-    y : number;
-    constructor(x:number,y:number){
-        this.x = x
-        this.y = y
-
-    }
-}
-
-class Name{
-    name! : string
-}
-const name  = new Name()
-name.name = 'ajsica'
 export default Todo;
