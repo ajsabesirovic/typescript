da2a4eac9a5400b92f2d9e8676a13e5ccce731fe
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-03-12 14:22:04 +0100

    removing a todo

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,27 +1,33 @@
-import { useState } from 'react';
-import './App.css';
-import NewTodo from './components/NewTodo';
-import Todos from './components/Todos';
-import Todo from './models/todo';
+import { useState } from "react";
+import "./App.css";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  const [todos,setTodos] = useState<Todo[]>([])
-  // const todos = [
-  //   new Todo('Learn react'),
-  //   new Todo('Learn ts')
-  // ]
-  const addTodo = (todoText : string) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (todoText: string) => {
     const newTodo = new Todo(todoText);
-    setTodos( (prevState) => {
-      return prevState.concat(newTodo)
-    })
-    // todos.push(new Todo(text))
-  }
+    setTodos((prevState) => {
+      // return prevState.concat(newTodo);
+      return [
+        ...prevState,
+        newTodo
+      ]
+    });
+  };
+
+  const removeTodo = (id: string) => {
+    setTodos((prevState) => {
+      return prevState.filter((todo) => todo.id !== id);
+    });
+  };
 
   return (
     <div>
-      <NewTodo onAddTodo={addTodo}/>
-      <Todos items={todos}/>
+      <NewTodo onAddTodo={addTodo} />
+      <Todos items={todos} rmvTodo={removeTodo} />
     </div>
   );
 }
