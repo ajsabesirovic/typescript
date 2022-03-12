73536f5f521df903389f286aa890b9a4fbebb6f5
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-03-12 13:13:36 +0100

    working with refs and useRef

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,11 +1,19 @@
+import { useRef } from "react";
 const NewTodo = () => {
+
+    const todoInputRef = useRef<HTMLInputElement>(null);
+
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+        const enteredText = todoInputRef.current?.value
+        if(enteredText?.trim().length === 0){
+            return 
+        }
     }
 
   return <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo text</label>
-      <input id="text" type="text" />
+      <input id="text" type="text" ref={todoInputRef} />
       <button>Add todo</button>
   </form>
 }
--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -8,6 +8,7 @@ const Todos: React.FC<{items: Todo[]}> = (props) => {
       {props.items.map((item) => (
         <TodoItem key={item.id} text={item.text}/>
       ))}
+      <h1>Ajsa glupa si</h1>
     </ul>
   );
 };
